43d60117a9ee4cc04ad42a2b3e90ed1bef716bd1
Kevin Hosford <kevehosford@gmail.com>
2019-08-19 10:19:25 -0500

    handle empty string

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,28 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Go")
-	want := "Hello, Go!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	// add an assertion function
+	assertTestOutput := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q wanted %q", got, want)
+		}
 	}
+
+	// add a Test.Run for returning name
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Go")
+		want := "Hello, Go!"
+
+		assertTestOutput(t, got, want)
+	})
+
+	// add a Test.Run for default meesage
+	t.Run("defaulting to world", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+
+		assertTestOutput(t, got, want)
+	})
 }
