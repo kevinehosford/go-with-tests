07d089bf9b92c582482043733f68bf91183d644b
Kevin Hosford <kevehosford@gmail.com>
2019-09-23 10:42:39 -0500

    Add reflection

---
+++ reflection/reflection.go
@@ -0,0 +1,63 @@
+package reflection
+
+import "reflect"
+
+// walk ...
+func walk(x interface{}, fn func(input string)) {
+	// val := reflect.ValueOf(x)
+
+	// if val.Kind() == reflect.Ptr {
+	// 	val = val.Elem()
+	// }
+
+	val := getValue(x)
+
+	// if val.Kind() == reflect.Slice {
+	// 	for i := 0; i < val.Len(); i++ {
+	// 		walk(val.Index(i).Interface(), fn)
+	// 	}
+	// 	return
+	// }
+
+	// for i := 0; i < val.NumField(); i++ {
+	// 	field := val.Field(i)
+
+	// 	switch field.Kind() {
+	// 	case reflect.String:
+	// 		fn(field.String())
+	// 	case reflect.Struct:
+	// 		walk(field.Interface(), fn)
+	// 	}
+	// }
+
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
+}
---
+++ reflection/reflection_test.go
@@ -0,0 +1,115 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Pointers to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				Profile{33, "London"},
+				Profile{44, "New Orleans"},
+			},
+			[]string{"London", "New Orleans"},
+		},
+		{
+			"Arrays",
+			[2]Profile{
+				Profile{33, "London"},
+				Profile{44, "New Orleans"},
+			},
+			[]string{"London", "New Orleans"},
+		},
+		{
+			"Maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
+
+func assetContains(t *testing.T, needle string, haystack []string) {
+	contains := false
+
+	for _, got := range haystack {
+		if got == needle {
+			contains = true
+		}
+	}
+
+	if !contains {
+		t.Errorf("Expected %v to contain %q but it didn't", haystack, needle)
+	}
+}
